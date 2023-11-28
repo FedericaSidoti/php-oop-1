76b062bb85cc7524ca525951af894eae62126fd0
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-28 16:05:45 +0100

    ADD- class and 3 instances

--- index..php
+++ index..php
@@ -1,5 +1,20 @@
 <?php
+class Production {
+    public $title; 
+    public $language;
+    public $rating; 
 
+    public function __construct($name, $lang, $vote) {
+        $this -> title = $name; 
+        $this -> language = $lang; 
+        $this -> rating = $vote; 
+    }
+};
+
+$movie_TPB = new Production("La Storia Fantastica - The Princess Bride", "Inglese", "8" );
+// var_dump($movie_TPB)
+$movie_Sdust = new Production("Stardust", "Inglese", "7");
+$movie_MR = new Production("Moulin Rouge!", "Americano", "9" ); 
 ?>
 
 <!DOCTYPE html>
