da3c6f6696cdad50e72a8a0bb5cebecd7bff7871
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-28 16:25:17 +0100

    ADD- setter in rating

--- index.php
+++ index.php
@@ -3,19 +3,35 @@ class Production {
     public $title; 
     public $language;
     public $rating; 
-
+    
     public function __construct($name, $lang, $vote) {
         $this -> title = $name; 
         $this -> language = $lang; 
-        $this -> rating = $vote; 
+        $this -> setRating($vote); 
     }
+    public function setRating($vote){
+        if (is_numeric($vote) && $vote >= 0) {
+            $this->rating = intval($vote);
+        } else {
+        $this->rating = null;
+        var_dump('Votazione non disponibile');
+        }
+    }
+    public function getRating() {
+        return $this->rating ; 
+    }
+    
 };
 
 $movie_TPB = new Production("La Storia Fantastica - The Princess Bride", "Inglese", "8" );
-// var_dump($movie_TPB)
+
 $movie_Sdust = new Production("Stardust", "Inglese", "7");
 $movie_MR = new Production("Moulin Rouge!", "Americano", "9" ); 
+
+
+var_dump($movie_TPB, $movie_Sdust, $movie_MR);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
