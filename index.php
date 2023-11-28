470ddca5a0459298cae8ee328548cd055ec6f3d0
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-28 17:18:51 +0100

    ADD- style and minor correction

--- index.php
+++ index.php
@@ -31,6 +31,9 @@ $movie_TPB = new Production("La Storia Fantastica - The Princess Bride", "Ingles
 
 $movie_Sdust = new Production("Stardust", "Inglese", "7");
 $movie_MR = new Production("Moulin Rouge!", "Americano", "9" ); 
+$movie_J = new Production("Jumanji", "Inglese", "9");
+$movie_C = new Production("Casper", "Inglese", "7");
+
 
 
 // var_dump($movie_TPB, $movie_Sdust, $movie_MR);
@@ -40,6 +43,8 @@ $movies = [
     $movie_TPB, 
     $movie_Sdust,
     $movie_MR,
+    $movie_J,
+    $movie_C,
 ];
 // var_dump($movies)
 ?>
@@ -51,24 +56,34 @@ $movies = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PhP Productions</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Lista dei miei film preferiti</h1>
-    <ul>
-        <?php
-            foreach($movies as $movie) {
-                ?>
-                <li>
-                    <ul>
-                        <li><?= $movie->title ?></li>
-                        <li><?= $movie->language ?></li>
-                        <li><?= $movie->getRating() ?></li>
-                        <li><?= $movie->description() ?></li>
-                    </ul> 
-                </li>
-            <?php
-            }
-        ?>
-    </ul>
+    <h1 class="text-center">Lista dei miei film preferiti</h1>
+    <div class="container">
+        <table class="table table-primary">
+        <thead>
+            <tr>
+                <th class= "col-4" scope="col">Titolo</th>
+                <th class= "col-2" scope="col">Lingua</th>
+                <th class= "col-1" scope="col">Voto</th>
+                <th class= "col-5" scope="col">Descrizione</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach($movies as $movie) { ?>
+            <tr>
+                <td><?= $movie->title ?></td>
+                <td><?= $movie->language ?></td>
+                <td><?= $movie->getRating() ?></td>
+                <td><?= $movie->description() ?></td>
+            </tr>
+            <?php  }  ?>
+        </tbody>
+    </table>
+    </div>
+
+
 </body>
 </html>
