13e92bdb94811ef88365063617c914ffe7a37edc
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-29 15:58:26 +0100

    FIX- names in arrays

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/Models/Production.php';
 require_once __DIR__ . '/Models/Movie.php';
 
 $movie_TPB = new Movie("La Storia Fantastica - The Princess Bride", "Inglese", "8", "10000", "97" );
-
 $movie_Sdust = new Movie("Stardust", "Inglese", "7", "20000", "120");
 $movie_MR = new Movie("Moulin Rouge!", "Americano", "9", "18000", "110" ); 
 $movie_J = new Movie("Jumanji", "Inglese", "9", "190000", "100");
@@ -15,7 +14,7 @@ $movie_C = new Movie("Casper", "Inglese", "7", "200000", "80");
 // var_dump($movie_TPB, $movie_Sdust, $movie_MR);
 
 
-$movies = [
+$productions = [
     $movie_TPB, 
     $movie_Sdust,
     $movie_MR,
@@ -49,12 +48,12 @@ $movies = [
             </tr>
         </thead>
         <tbody>
-        <?php foreach($movies as $movie) { ?>
+        <?php foreach($productions as $production) { ?>
             <tr>
-                <td><?= $movie->title ?></td>
-                <td><?= $movie->language ?></td>
-                <td><?= $movie->getRating() ?></td>
-                <td><?= $movie->description() ?></td>
+                <td><?= $production->title ?></td>
+                <td><?= $production->language ?></td>
+                <td><?= $production->getRating() ?></td>
+                <td><?= $production->description() ?></td>
                 <td>info</td>
             </tr>
             <?php  }  ?>
